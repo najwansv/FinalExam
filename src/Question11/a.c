a0eed3162be498fb8c3e4238470080f56b730a7c
najwansv <79251871+najwansv@users.noreply.github.com>
2021-12-13 10:32:00 +0700

    Update a.c

--- src/Question11/a.c
+++ src/Question11/a.c
@@ -5,9 +5,9 @@ int main(){
     int x, i, y;
     float large, small;
 
-    float rainfall[12];
+    float rainfall[12]; // A
 
-    for(i=0; i<12; i++){
+    for(i=0; i<12; i++){ // B untill lane 13
         printf("Month %i : ", i+1);
         scanf("%f",&rainfall[i]);
     }
@@ -18,10 +18,10 @@ int main(){
         sum += rainfall[x];        
     }
 
-    average = sum / 12;
+    average = sum / 12; // C until lane 22
     printf("Average of this year = %.2f\n", average);
 
-    large=small=rainfall[0];
+    large=small=rainfall[0]; // D until lane 39
 
     for(y=0;y<12;y++)
     {
